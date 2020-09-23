da15aee76b6d940566d74e7a2b9788c011bf3de2
Alison Manning <alisonlaurenmanning@gmail.com>
2020-09-23 09:31:41 -0500

    Update index.js
    adding in spices, sugar, fats, powder and liquids arrays.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const meat = ["Chicken", "Beef", "Pork", "Bacon", "Beef Brisket", "Beef Fillet", "Chicken Breast", "Chicken Breasts", "Chicken Legs", "Chicken Thighs", "Chorizo", "Doner Meat", "Italian Fennel Sausages", "Lamb", "Lamb Loin Chops", "Lamb Mince", "Lean Minced Beef", "Parm Ham", "Duck", "Duck Legs", "Lamb Leg", "Lamb Shoulder", "Minced Beef", "Turkey Mince", "Lamb Kidney", "Beef Kidney", "Sausages", "Ham", "Haddock", "Smoked Haddock", "Beef Shin", "Minced Pork", "Ground Beef", "Goat Meat", "Tripe", "Ground Pork", "Pork Chops", "Kielbasa", "Polish Sausage"]
 
 const seaFood = ["Salmon", "King Prawns", "Mackerel", "Prawns", "Raw King Prawns", "Oysters", "White Fish", "White Fish Fillets", "Mussels", "Monkey Fish", "Squid", "Baby Squid", "Clams", "Tiger Prawns", "Cod", "Salt Cod", "Smoked Salmon", "Herring"]
+
+const spices = ["Basil", "Basil leaves", "Black Pepper", "Cardamom", "Chili Powder", "Cilantro", "Cinnamon", "Cloves", "Coriander Seeds", "Cumin", "Dill", "Dried Oregano", "Fajita Seasoning", "Fresh Thyme", "Garlic", "Ginger", "Italian Seasoning", "Lemon Zest", "Minced Garlic", "Mint", "Nutmeg", "Onion Salt", "Oregano", "Paprika", "Red Chilli Powder", "Salt", "Sea Salt", "Sage", "Tumeric", "Rosemary", ]
+      
+const sugars = ["Caster Sugar", "Brown Sugar", "Chocolate Chips", "Coco Sugar", "Cocoa", "Dark Brown Sugar", "Granulated Sugar", "Greek Yogurt", "Plain Chocolate", "Sugar", "White Chocolate Chips", "Icing Sugar", "Carmel", "Milk Chocolate", "Dark Chocolate", "Powdered Sugar"]
+      
+const fats = ["Butter", "Chicken Stock", "Coconut Cream", "Cocunut Milk", "Cheddar Cheese", "Cheese", "Colby Jack Cheese", "Condensed Milk", "Cream", "Double Cream", "Egg White", "Egg Yolks", "Egg", "Feta", "Full Fat Yogurt", "Gouda Cheese", "Heavy Cream", "Milk", "Oils", "Olive Oil", "Paresan Cheese", "Peanut Butter", "Shredded Mexican Cheese", "Sour Cream", "Sunflower Oil", "Vegetable Oil", "Whole Milk", "Yogurt", "Cream Cheese", "Mozzerela", "Egg", "Brie", "Canola Oil", "Almond Milk", ]
+      
+const powder = ["Baking Powder", "Bicarbonate Of Soda", "Corn Starch"]
+
+const liquids = ["Apple Cider Vinegar", "Dark Soy Sauce", "Dry White Wine", "Enchilada Sauce", "English Mustard", "Extra Virgin Olive Oil", "Garlic Sauce", "Golden Syrup", "Honey", "Hotsauce", "Lemon Juice", "Mustard", "Raspberry Jam", "Red Wine", "Soy Sauce", "Tomato Ketchup", "Vanilla", "Vanilla Extract", "Vinegar", "White Vinegar", "White Wine","Barbeque Sauce", "Red Wine Vinegar", "Tomato Sauce", "Maple Syrup", "Tabasco Sauce", "White Wine Vinegar", "Mayonnaise", ]
